ac22ba8a74f4089319eb5a2b663425c9084af58e
Ajujuba <60891489+Ajujuba@users.noreply.github.com>
2020-03-30 13:46:25 -0300

    Aula 5
    Login

--- public/index.php
+++ public/index.php
@@ -2,19 +2,17 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-
 use Alura\Cursos\Controller\InterfaceControladorRequisicao;
 
-
-$caminho = $_SERVER['PATH_INFO']; //pega o caminho das rotas
+$caminho = $_SERVER['PATH_INFO'];
 $rotas = require __DIR__ . '/../config/routes.php';
 
-if(!array_key_exists($caminho, $rotas)){ // verifica se existe o caminho q peguei nas rotas e n entra no array se tiver
+if (!array_key_exists($caminho, $rotas)) {
     http_response_code(404);
-    exit();  
+    exit();
 }
 
-$classeControladora = $rotas[$caminho]; //aqui pega o valor da rota, pra onde ela vai
-$controlador = new $classeControladora(); //com php se eu tenho numa variável o valor de uma string que é o nome de uma variável pode instanciar assim
+$classeControladora = $rotas[$caminho];
+/** @var InterfaceControladorRequisicao $controlador */
+$controlador = new $classeControladora();
 $controlador->processaRequisicao();
-
